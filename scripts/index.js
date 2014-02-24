ebd9e859c99f76a7ef0bfb6cae377a977291fa35
garbrand <garbrand@gmail.com>
2014-02-24 14:25:43 +0100

    Darn, uncommented the horrible location hack...

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,9 @@
 	// TODO: Why this hard redirection? This way it is impossible to debug locally.
 	// configuration
 
-    // if (window.location.host === 'hudson.local:8080' || window.location.host === '') {
-    //     window.location.href = 'http://hudson.local/job/dashboard/ws/index.html';
-    // }
+    if (window.location.host === 'hudson.local:8080' || window.location.host === '') {
+        window.location.href = 'http://hudson.local/job/dashboard/ws/index.html';
+    }
 
 	var basePath = 'http://hudson.local';
 	var viewName = 'IPE Story Jobs';
