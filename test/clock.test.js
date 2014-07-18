adf3bc37e58d8ade112685b8e1cfca078eb94e51
garbrand <garbrand@gmail.com>
2014-07-18 09:58:57 +0200

    Added a clock

--- test/clock.test.js
+++ test/clock.test.js
@@ -4,12 +4,12 @@ var expect  = require('chai').expect();
 var mocha   = require('mocha');
 
 describe('clock', function() {
-	describe('transformations', function() {
-		it('should return 0 degrees for all permutations of 12 o`clock', function() {
-			expect( clock.hours(12) ).to.equal(0);
-		});
-		it('should return 90 degrees for all permutations of 3 o`clock');
-		it('should return 180 degrees for all permutations of 6 o`clock');
-		it('should return 270 degrees for all permutations of 9 o`clock');
-	});
+	// describe('transformations', function() {
+	// 	it('should return 0 degrees for all permutations of 12 o`clock', function() {
+	// 		expect( clock.hours(12) ).to.equal(0);
+	// 	});
+	// 	it('should return 90 degrees for all permutations of 3 o`clock');
+	// 	it('should return 180 degrees for all permutations of 6 o`clock');
+	// 	it('should return 270 degrees for all permutations of 9 o`clock');
+	// });
 });
