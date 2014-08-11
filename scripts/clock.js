dd5a03e61c7a883e277b2c60efa384ad6a1de202
Garbrand van der Molen <garbrand@garbrandpro.local>
2014-08-11 16:54:53 +0200

    Added quotes and a template

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,48 +1,87 @@
-var view = {
-	rotate: function(el, deg) {
-		el.setAttribute("transform", "rotate("+deg+" 100 100)");
-	},
+function Clock(id, o) {
+	var offset = o || 0;
+	var clock  = document.querySelector(id);
 
-	// TODO: Make this a factory where `offset` is already set
-	// TOOD: This procedure has two functionalities: rotating hands and setting day/night. Split it.
-	animate: function(id, date, offset) {
-		var o = offset || 0;
-		var t = date.getHours() + o;
-
-		view.rotate( document.querySelector(id + ' .hourhand'),   t * 30 );
-		view.rotate( document.querySelector(id + ' .minutehand'), date.getMinutes() * 6 );
-		view.rotate( document.querySelector(id + ' .secondhand'), date.getSeconds() * 6 );
-
-		function night() {
-			document.querySelector(id).classList.add('night');
-		}
-
-		function day() {
-			document.querySelector(id).classList.remove('night');
-		}
-
-		return (t <= 7 || t >= 20) ? night() : day();
+	function t(offset) {
+		var d = new Date();
+		d.setHours(d.getHours() + offset);
+		return d;
 	}
-};
 
-// Time to Degrees:
-var ttd = {
-	s: function(d) { return d.getSeconds() * 6; },
-	m: function(d) { return d.getMinutes() * 6; },
-	h: function(d) { return d.getHours()   * 30; }
-};
+	function rotate(el, deg) {
+		return el.setAttribute("transform", "rotate("+deg+" 100 100)");
+	}
 
+	var h = {
+		hour: function(time) {
+			return ( time.getHours() * 30 + (time.getMinutes() * 0.5) );
+		},
+		min: function(time) { return time.getMinutes() * 6; },
+		sec: function(time) { return time.getSeconds() * 6; }
+	};
 
-function draw() {
-	var d = new Date();
-	
-	view.animate('#Baarn', d);
-	view.animate('#Markham', d, -6);
-	view.animate('#Seoul', d, 7);
-	view.animate('#Beijing', d, 6);
+	// TODO: decide if this is an object or a function
+	return function Clock() {
+		var time   = t(offset);
+
+		function set(clock, time) {
+			rotate( clock.querySelector('.hourhand'),   h.hour(time) );
+			rotate( clock.querySelector('.minutehand'), h.min(time) );
+			rotate( clock.querySelector('.secondhand'), h.sec(time) );
+		}
+
+		function dim(clock, time) {
+			var h = time.getHours();
+			return (h <= 7 || h >= 20) ? clock.classList.add('night') : clock.classList.remove('night');
+		}
+
+		function render() {
+			set(clock, time);
+			dim(clock, time);
+		}
+
+		var api = {
+			render: render,
+			time: time
+		};
+
+		return api;
+	};
 }
 
-draw();
-setInterval(draw, 1000);
+function Template(location, offset) {
+	// Loads the inner HTML of the template, replaces some placeholders.
+	// Currently grabs <script id="clock-template">
 
-(typeof module !== 'undefined') && (module.exports = time);
+	var t = document.getElementById('clock-template').innerHTML;
+
+	function calcOffset(offset) {
+		return (offset <0) ? offset *-1 + ' uur eerder' : offset + ' uur later';
+	}
+
+	var r = t
+		.replace('{{id}}', location)
+		.replace('{{location}}', location)
+		.replace('{{offset}}', calcOffset(offset))
+	;
+
+	return r;
+}
+
+
+// Use the Clock:
+var clocks = [
+	Clock('#Baarn',    0),
+	Clock('#Markham', -6),
+	Clock('#Seoul',    7),
+	Clock('#Beijing',  6)
+];
+
+function setClocks() {
+	clocks.forEach(function(clock) {
+		clock().render();
+	});
+}
+
+setClocks();
+setInterval(setClocks, 1000);
